8e2a01bcf6e97b13f83b262569ff9481114e9384
MashoorNEGI <ayushdeepnegi@gmail.com>
2023-08-26 18:24:40 +0530

    removing unused controllers

--- controllers/comment.js
+++ controllers/comment.js
@@ -58,20 +58,4 @@ const editcomment = async (req, res) => {
 }
 
 
-const location = async (req, res) => {
-    const { id: _id } = req.params
-    const { address } = req.body
-    if (!mongoose.Types.ObjectId.isValid(_id)) {
-        return res.status(404).send("Unavailable..");
-    }
-    try {
-        const updateLike = await comment.findByIdAndUpdate(_id, {
-            $set: { "address": address }
-        })
-        res.status(200).json(updateLike)
-    } catch (error) {
-        res.status(400).json("error:", error)
-
-    }
-}
-module.exports = { postcomment, editcomment, deletecomment, getcomment, location }
+module.exports = { postcomment, editcomment, deletecomment, getcomment }
